fa8901195bbdea7d5da3aad13b97d1c8a44db39c
Tomás Morales <tomasmrivero05@gmail.com>
2025-05-04 23:07:42 -0500

    Adds UnableToLoadApiKey custom exception for API key errors

--- src/com/currencyswapp/exception/UnableToLoadApiKey.java
+++ src/com/currencyswapp/exception/UnableToLoadApiKey.java
@@ -2,6 +2,10 @@ package com.currencyswapp.exception;
 
 public class UnableToLoadApiKey extends RuntimeException {
 
+    public UnableToLoadApiKey(String message) {
+        super(message);
+    }
+
     public UnableToLoadApiKey(String message, Throwable cause) {
         super(message, cause);
     }
